7eaf4a67d26b3d78c0b7b772e34275ec8a8a158e
Charles Cunningham <charles.cunningham@outlook.com>
2019-04-18 15:11:31 +0200

    added build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Create build dir..."
+mkdir sea_build
+cd mkdir
+
+echo "Create build env with CMake..."
+cmake ./../secure-element-abstraction/ -DTARGET_GROUP=v2 -DCMAKE_TOOLCHAIN_FILE=../toolchain-rpi.cmake
+
+echo "Build libseadyn.so..."
+make
+cp ./src/libseadyn.so ../libseadyn.so
+
+cd ./..
