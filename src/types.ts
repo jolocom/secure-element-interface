36d4bd75d47ffc5d06921615ed1f9420bd2b856b
Charles Cunningham <charles.cunningham@outlook.com>
2019-04-24 16:11:41 +0200

    changed to a class based structure

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,5 @@
 
-export interface SecureElement {
-  init: () => {}
+export interface ISecureElement {
   getRandom: (len: number) => Buffer
   getPublicKey: (index: number) => Buffer
   sign: (key: number, message: Buffer) => Buffer
